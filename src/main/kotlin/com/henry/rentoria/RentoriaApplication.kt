4ed71e456ad2b65e0d0ba255eed8274995ba1f53
Henry L <jianlisword@gmail.com>
2020-08-01 21:13:12 -0700

    initial commit to initialize the Spring Boot application

---
+++ src/main/kotlin/com/henry/rentoria/RentoriaApplication.kt
@@ -0,0 +1,11 @@
+package com.henry.rentoria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RentoriaApplication
+
+fun main(args: Array<String>) {
+	runApplication<RentoriaApplication>(*args)
+}
---
+++ src/test/kotlin/com/henry/rentoria/RentoriaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.henry.rentoria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RentoriaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
